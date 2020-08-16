a0e582c245b05142a97fde396e7299cceab44484
Gavin S <46635587+blue2cat@users.noreply.github.com>
2020-08-16 13:36:28 -0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'WXcheck website',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Website for WXcheck',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
